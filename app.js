0578d135904e3aabce54129f25760a349fa953e6
Dhruv Mittal <dhruvm14@gmail.com>
2021-02-13 14:45:32 +0530

    Home Page Redesign

--- app.js
+++ app.js
@@ -16,30 +16,29 @@ if ($(".navbar").length > 0) {
 
 //TIMER LOGIC
 $(document).ready(function () {
-const webinar = "21 Nov 2020";
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minsEl = document.getElementById("mins");
-const secondsEl = document.getElementById("seconds");
-function countdown() {
-  const webinarDate = new Date(webinar);
-  const curDate = new Date();
+  const webinar = "21 Nov 2020";
+  const daysEl = document.getElementById("days");
+  const hoursEl = document.getElementById("hours");
+  const minsEl = document.getElementById("mins");
+  const secondsEl = document.getElementById("seconds");
+  function countdown() {
+    const webinarDate = new Date(webinar);
+    const curDate = new Date();
 
-  const tSeconds = (webinarDate - curDate) / 1000;
-  const days = Math.floor(tSeconds / 3600 / 24);
-  const hours = Math.floor(tSeconds / 3600) % 24;
-  const mins = Math.floor(tSeconds / 60) % 60;
-  const seconds = Math.floor(tSeconds) % 60;
+    const tSeconds = (webinarDate - curDate) / 1000;
+    const days = Math.floor(tSeconds / 3600 / 24);
+    const hours = Math.floor(tSeconds / 3600) % 24;
+    const mins = Math.floor(tSeconds / 60) % 60;
+    const seconds = Math.floor(tSeconds) % 60;
 
-  daysEl.innerHTML = days;
-  hoursEl.innerHTML = hours;
-  minsEl.innerHTML = mins;
-  secondsEl.innerHTML = seconds;
-}
-// countdown();
-setInterval(countdown, 1000);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+  }
+  // countdown();
+  setInterval(countdown, 1000);
 });
-
 
 function bootstrapAlert() {
   alert("PORTFOLIO WILL BE LIVE SOON !!!");
@@ -51,16 +50,26 @@ $(document).ready(function () {
     if (value == "all") {
       $(".pic").show(1000);
     } else {
-      $(".pic").not("." + value).hide(1000);
-      $(".pic").filter("." + value).show(1000);
+      $(".pic")
+        .not("." + value)
+        .hide(1000);
+      $(".pic")
+        .filter("." + value)
+        .show(1000);
     }
   });
   $(".galleryList").click(function () {
-    $(this).addClass('active').siblings().removeClass('active');
-  })
+    $(this).addClass("active").siblings().removeClass("active");
+  });
 });
 
-$(".img-link").click(function(){
-  const val = $(this).find(".pic").find('img').attr('src');
-  $(".modal-content").find('img').attr('src',val);
-})
+$(".img-link").click(function () {
+  const val = $(this).find(".pic").find("img").attr("src");
+  $(".modal-content").find("img").attr("src", val);
+});
+
+function toggle_div() {
+  var element = document.getElementById("sidebar");
+  element.classList.toggle("active");
+}
+AOS.init();
